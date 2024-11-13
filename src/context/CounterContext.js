b6578812c4fd78461e0e46cf516a784bc095927a
Tryvia Sua Frota Mais Inteligente <hiensen.cdl@gmail.com>
2024-11-13 20:26:50 -0300

    creating Provider and changing Context API

---
+++ src/context/CounterContext.js
@@ -0,0 +1,20 @@
+// 1 - criar context, onde haverá mudanças de valores
+import {  createContext, useState } from "react";
+
+// eslint-disable-next-line no-undef
+export const CounterContext = createContext();
+
+
+
+//2 - criar provider
+
+export const CounterContextProvider = ({children}) => {
+    const [counter, setCounter] = useState(5)
+
+    return(
+        // eslint-disable-next-line react/jsx-no-undef
+        <CounterContext.Provider value={{counter, setCounter}}>
+            {children}
+        </CounterContext.Provider>
+    )
+}
